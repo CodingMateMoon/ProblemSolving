36f8f11da41b40b17c2262257aebbd5003fe8d16
JangHyunMoon <abc9529@naver.com>
2019-01-28 09:32:48 +0900

    Update NumberTheory_Beehive.java

--- baekjoon/Basic/NumberTheory_Beehive.java
+++ baekjoon/Basic/NumberTheory_Beehive.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class NumberTheory_Beehive {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(getMaxInLevel(n));
+	}
+	
+	public static int getMaxInLevel(int num) {
+		if (num == 1)
+			return 1;
+		int sum = 1;
+		int level = 1;
+		while (sum < num) {
+			sum += 6 * level;
+			level++;
+		}
+		return level;
+	}
 }
