830f1dd01378da70e9e42866898d2582fd2997c5
JangHyunMoon <abc9529@naver.com>
2019-02-18 00:31:02 +0900

    Update NumberTheory_CombinationPascal.java

--- baekjoon/Basic/NumberTheory_CombinationPascal.java
+++ baekjoon/Basic/NumberTheory_CombinationPascal.java
@@ -2,28 +2,16 @@ import java.util.Scanner;
 
 public class NumberTheory_CombinationPascal {
 
-	/*
-	 예제 입력 (5 C 2 ) n C m
-	5 2
-	예제 출력
-	10
-	 * */
+	static int[][] arr;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		double result = 1;
-		if (n == 0 || m == 0 || n == 1) {
-			System.out.println(1);
-			return;
-		}
 		
-		for (int i = 0; i < m; i++) {
-			result *= (double) (n - i) / (m - i);
-		}
-		System.out.println((int) result);
+		arr = new int[n + 1][n + 1];
+		System.out.println(getPascal(n, m));
 	}
 	
 	public static int getPascal(int n, int m) {
@@ -37,8 +25,10 @@ public class NumberTheory_CombinationPascal {
 			return arr[n][m];
 		} 
 		
+		
+		
 		arr[n][m] = getPascal(n - 1, m - 1) + getPascal(n - 1, m);
 		return arr[n][m];
+		
 	}
-
 }
